b46e8fc8533ed2e5f0fa597e77f2dab786745475
Sahil Bhingradiya <sahilbhingradia28@gmail.com>
2025-05-04 21:20:48 +0530

    update CORS

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://content-creator-frontend.web.app/'], // Allow only this origin
+    origin: ['http://localhost:5173', 'https://content-creator-frontend.web.app'], // Allow only this origin
 })); // Enable CORS
 app.use(express.json());
 
